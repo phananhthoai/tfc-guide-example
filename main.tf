8bf4e350917f0b42ccf02a409803a4393645e6ea
khanh <k3nkukhanh@gmail.com>
2023-12-08 03:52:38 +0000

    edit

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ data "aws_ami" "ubuntu" {
 data "google_compute_image" "my_image" {
   family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
-  name    = "ubuntu-2004-focal-v20231130"
 }
 
 resource "google_compute_instance" "default" {
