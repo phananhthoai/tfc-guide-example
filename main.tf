70a92eeb11316652b81479d5b16efbb7daf617f6
khanh <k3nkukhanh@gmail.com>
2024-11-21 11:56:18 +0000

    edit

--- main.tf
+++ main.tf
@@ -10,25 +10,25 @@ provider "aws" {
 #  zone    = "us-central1-a"
 #}
 
-provider "cloudflare" {  
-}
+# provider "cloudflare" {  
+# }
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
 #data "google_compute_image" "my_image" {
 #  family  = "ubuntu-2004-lts"
@@ -80,34 +80,33 @@ output "peering_connection_id" {
 }
 
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = data.aws_subnets.vpc_subnets.id'
-  instance_type = t2.micro
+# resource "aws_instance" "ubuntu" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+#   subnet_id = data.aws_subnets.vpc_subnets.id'  
 
-  tags = {
-    Name = var.instance_name
-  }
-}
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
 
 
-resource "aws_eip" "lb" {
- instance = aws_instance.ubuntu.id
- domain   = "vpc"
-}
+# resource "aws_eip" "lb" {
+#  instance = aws_instance.ubuntu.id
+#  domain   = "vpc"
+# }
 
-data "cloudflare_zone" "main" {
-  name = var.zone_name
-}
+# data "cloudflare_zone" "main" {
+#   name = var.zone_name
+# }
 
-resource "cloudflare_record" "aws" {
-  zone_id = data.cloudflare_zone.main.id
-  name    = "k8s"
-  value   = aws_eip.lb.public_ip
-  type    = "A"
-  proxied = false
-}
+# resource "cloudflare_record" "aws" {
+#   zone_id = data.cloudflare_zone.main.id
+#   name    = "k8s"
+#   value   = aws_eip.lb.public_ip
+#   type    = "A"
+#   proxied = false
+# }
 
 #resource "cloudflare_record" "google" {
 #  zone_id = data.cloudflare_zone.main.id
